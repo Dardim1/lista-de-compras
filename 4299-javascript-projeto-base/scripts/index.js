2df8d86a88be07dae7d0a0dcfef89ffdf5c26f98
Dardim1 <136619471+Dardim1@users.noreply.github.com>
2025-02-26 02:55:07 -0300

    Add files via upload

---
+++ 4299-javascript-projeto-base/scripts/index.js
@@ -0,0 +1,17 @@
+import { criarItemDaLista } from "./criarItemDaLista.js";
+import verificarListaVazia from "./verificarListaVazia.js";
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+
+
+botaoAdicionar.addEventListener("click" , (evento) => {
+    evento.preventDefault();
+    const itemDaLista = criarItemDaLista()
+    listaDeCompras.appendChild(itemDaLista)
+    verificarListaVazia(listaDeCompras);
+})
+
+
+verificarListaVazia(listaDeCompras);
+ 
